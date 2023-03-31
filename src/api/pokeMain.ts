36ead7fe99da1f3ba273009c583ca3b4de03baf1
kimhw7 <khw786678@gmail.com>
2023-03-31 22:24:04 +0900

    delete import

--- src/api/pokeMain.ts
+++ src/api/pokeMain.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 import request from "./instance";
-import type { AxiosResponse } from "axios";
-import type { IPokemon } from "../type/pokemon";
 
 export const getPokeList = async (limit: number, offset: number) => {
   const response: any = request.get(`pokemon?limit=${limit}&offset=${offset}`);
